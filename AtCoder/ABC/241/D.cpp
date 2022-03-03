9c60324eaca80a3dd69399ed45ce89b4e464ffeb
sky8c9 <sky8cloud9@gmail.com>
2022-03-03 00:40:25 -0800

    241d

---
+++ AtCoder/ABC/241/D.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+using ll = long long;
+
+void task() {
+	int q;
+	cin >> q;
+
+	multiset<ll> nums;
+	for(int i = 0; i < q; i++) {
+		int type;
+		ll x;
+		cin >> type >> x;
+
+		if (type == 1) {
+			nums.insert(x);
+		} else {
+			int k;
+			cin >> k;
+
+			multiset<ll>::iterator itr;
+			if (type == 2) {
+				itr = nums.upper_bound(x);
+				while(k > 0 && itr != nums.begin()) {
+					--itr;
+					--k;
+				}
+
+				//cout << (k > 0 ? -1 : *itr) << endl;
+			} else {
+				itr = nums.lower_bound(x);
+				if (itr != nums.end()) {
+					while(k > 1 && itr != prev(nums.end())) {
+						++itr;
+						--k;
+					}
+					--k;
+				}
+				//cout << (itr == nums.end() ? -1 : *itr) << endl;
+			}
+
+			if (k == 0) {
+				cout << *itr << endl;
+			} else {
+				cout << -1 << endl;
+			}
+		}
+	}
+	
+}
+
+int main() {
+	task();
+	return 0;
+}
